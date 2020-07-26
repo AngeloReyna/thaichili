a7f67b73ea304d3c71366f2e2bef0c896a804c21
AngeloReyna <63065867+AngeloReyna@users.noreply.github.com>
2020-07-26 09:35:34 -0700

    Add files via upload

---
+++ ThaiChili/thai.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    burger.addEventListener("click",() => {
+        nav.classList.toggle("nav-active");
+           //Animte the links
+    navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+            link.syle.animation = '';
+        }else {
+            link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 2}s';
+
+        }
+    });
+    });
+ 
+
+}
+
+navSlide();
